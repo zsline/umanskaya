5acf4981927c2335d4031c48d2c56ce7c3f7e01e
zsline <sergei.dereka@gmail.com>
2024-09-11 13:06:45 +0300

    all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+document.querySelector('.menu-btn').addEventListener('click', function(){
+    this.classList.toggle('open');
+    document.querySelector('.nav').classList.toggle('open');
+    document.querySelector('body').classList.toggle('noscroll');
+})
+
+
 if(document.querySelector('.cases__slider')){
     const swiper = new Swiper('.cases__slider', {
         loop: true,
